a4a8b316cc1b036d95fedf6c35df34af65fc8969
Hemant <davidbroddy1@gmail.com>
2024-11-27 14:23:50 +0530

    "Updated codegen.ts to use localhost GraphQL schema and refactored UserRecommendationCard.tsx component with new styles and layout"

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://ec2-65-0-105-48.ap-south-1.compute.amazonaws.com:8000/graphql",
+  // schema: "http://ec2-65-0-105-48.ap-south-1.compute.amazonaws.com:8000/graphql",
+  schema: "http://localhost:8000/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
--- components/UserRecommendationCard.tsx
+++ components/UserRecommendationCard.tsx
@@ -7,34 +7,41 @@ import Link from "next/link";
 
 export default function UserRecommendationCard() {
     const {user} = useCurrentUser();
+
+    
+
     if(user && user?.recommendations?.length === 0 || undefined)
         return null;
     return(
         <div>
-            <div className="bg-neutral-900 bg-opacity-40 p-2 w-full h-64 rounded-lg overflow-y-scroll no-scrollbar">
-                <h1 className="text-lg font-semibold border-b mb-4 w-fit mx-auto">Users you may know</h1>
+            <div className="flex flex-col items-center bg-gradient-to-br from-indigo-900 via-sky-950 to-violet-900 bg p-2 w-full hover:scale-95 duration-200 h-64 rounded-lg overflow-y-scroll no-scrollbar">
+                <h1 className="text-xl font-semibold border-b mb-4 w-fit mx-auto">You may know</h1>
                 {user && user.recommendations?.map((recommendedUser) => {
+                    const fullname = `${recommendedUser?.firstName} ${recommendedUser?.lastName}`;
                     return (
                         <div
                         key={recommendedUser?.id}
-                        className="bg-neutral-800 bg-opacity-60 flex items-center gap-2 rounded-md p-2"
+                        className="w-fit my-2 bg-gradient-to-br from-sky-500 via-teal-400 to-indigo-500 hover:scale-110 hover:-translate-y-2 duration-200 rounded-xl p-px "
                         >
-                            <Image
-                            src={recommendedUser?.profileImageURL || avatar}
-                            alt="user"
-                            width={50}
-                            height={50}
-                            className="rounded-full"
-                            />
-                            <div className="h-full">
-                                <p
-                                className="mb-2"
-                                >{recommendedUser?.firstName} {recommendedUser?.lastName}</p>
-                                <Link
-                                href={`/${recommendedUser?.id}`}
-                                className="bg-white text-black px-2 py-1 rounded-lg"
-                                >View</Link>
-                            </div>
+                            <Link
+                            href={`/${recommendedUser?.id}`}
+                            className="bg-neutral-950 duration-200 ease-linear bg-opacity-90 w-60 flex items-center justify-start gap-2 rounded-xl p-2">
+                                <Image
+                                src={recommendedUser?.profileImageURL || avatar}
+                                alt="user"
+                                width={50}
+                                height={50}
+                                className="rounded-full border p-px"
+                                />
+                                <div className="h-full flex flex-col items-start ">
+                                    <p className="">
+                                        {
+                                            fullname.length <= 15 ? fullname : `${fullname.slice(0, 15)}...`
+                                        }
+                                    </p>
+                                    <p className="text-gray-500">@username</p>
+                                </div>
+                            </Link>
                         </div>
                     )}
                 )}
