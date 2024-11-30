fddfefd5b43658b78fcf2e5f91731823189c990e
Hemant <davidbroddy1@gmail.com>
2024-11-30 10:47:39 +0530

    "Added back button and title to Bookmarks component"

--- components/Bookmarks.tsx
+++ components/Bookmarks.tsx
@@ -3,9 +3,13 @@ import Image from "next/image";
 import BookmarkCard from "./BookmarkCard";
 import { useGetUserBookmarks } from "@/hooks/user"
 import { Tweet } from "@/gql/graphql";
+import { FaArrowLeft } from "react-icons/fa6";
+import { useRouter }  from "next/navigation";
+
 
 export const Bookmarks = () => {
     const { data, isError, isLoading, bookmarks} = useGetUserBookmarks();
+    const router = useRouter();
 
     if(isLoading) return <p>Loading Bookmarks...</p>
     if(isError) return <p>Bookmarks ERROR</p>
@@ -13,6 +17,16 @@ export const Bookmarks = () => {
     
     return (
         <>
+            <div className="flex items-center gap-4 my-2">
+                <button
+                onClick={() => router.back()}
+                className="text-lg hover:bg-neutral-800 transition-all ease-linear p-2 m-2 rounded-full">
+                    <FaArrowLeft />
+                </button>
+                <p className="text-lg font-semibold">
+                    Bookmarks
+                </p>
+            </div>
             {bookmarks?.map((bookmark) =>
                 bookmark ? <BookmarkCard key={bookmark?.id} data={bookmark as Tweet} /> : null
             )}
